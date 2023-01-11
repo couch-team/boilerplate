09e6e77b79ff588c56447fe5f071c1a3d310796f
Muritala David <davmuri1414@gmail.com>
2023-01-11 18:48:42 +0100

    update

--- src/v1/modules/sample/services/sample.service.ts
+++ src/v1/modules/sample/services/sample.service.ts
@@ -1,13 +1,19 @@
-import { injectable } from "tsyringe";
+import { container, injectable } from "tsyringe";
+import SampleRepository from "../repositories/sample.repositories";
 
 @injectable()
 export default class SampleService {
+  public sampleRepo = container.resolve(SampleRepository);
+
   public async execute(value: string) {
+    
+    const sample = await this.sampleRepo.create({ value });
+
     return {
       status: true,
       message: "Sample service executed",
       data: {
-        value,
+        sample,
       },
     };
   }
